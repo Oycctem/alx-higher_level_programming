dbf62c72d6db3f27b81c5542a886f2ff96915596
Oycctem <ousstemm@gmail.com>
2023-10-31 15:54:55 -0700

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(10):
+    for number1 in range(number + 1, 10):
+        if number == 8 and number1 == 0:
+            print('89')
+        else:
+            print('{}{}'.format(number, number1), end=',')
