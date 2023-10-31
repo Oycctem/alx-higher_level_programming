009838602d4bd98b73d9401b3036454f694cf31f
Oycctem <ousstemm@gmail.com>
2023-10-31 16:45:58 -0700

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,9 @@
 def fizzbuzz():
     for number in range(1, 101):
         output = ''
-        if num % 3 == 0:
+        if number % 3 == 0:
             output += 'Fizz'
-        if num % 5 == 0:
+        if number % 5 == 0:
             output += 'Buzz'
         if not output:
             output = number
