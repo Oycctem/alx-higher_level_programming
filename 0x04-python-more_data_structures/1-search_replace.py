b8bfbe68c1d1d31aa72bed5b8e3981e5fe98f2ca
Oycctem <ousstemm@gmail.com>
2023-11-08 04:03:22 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    i = [replace if element == seach else element for element in my_list]
+    i = [replace if element == search else element for element in my_list]
     return (i)
