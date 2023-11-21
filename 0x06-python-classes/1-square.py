8c7e3266e4d05aaf0503205e95eaaae8800f5f5a
Oycctem <ousstemm@gmail.com>
2023-11-21 12:35:30 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defining a class named Squre"""
 
+
 class Square:
     """Defining the constructor for the Square class"""
     def __init__(self, size):
-        seld.__size = size
+        self.__size = size
