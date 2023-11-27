4dad8395e77fe7f42ac8d98d51161bb93ebd26eb
Oycctem <ousstemm@gmail.com>
2023-11-27 12:47:35 -0800

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """rectangle"""
+    def __init__(self, width=0, height=0):
+        """__init__: instance method"""
+        self.wdith = width
+        self.height = height
+
+    @property
+    def width(self):
+        """the getter function"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """the setter function"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """the getter function"""
+        return self._height
+
+    @height.setter
+    def height(self; value):
+        """the setter fucntion"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an interger")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
