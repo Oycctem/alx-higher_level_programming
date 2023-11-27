76de83b2a6fde8d0ff0669770ea6d06ded149e18
Oycctem <ousstemm@gmail.com>
2023-11-27 12:49:39 -0800

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     @property
     def width(self):
         """the getter function"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -26,7 +26,7 @@ class Rectangle:
     @property
     def height(self):
         """the getter function"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self; value):
