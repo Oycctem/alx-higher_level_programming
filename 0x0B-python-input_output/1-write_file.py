e8afeec201d5579bd57669e4aeded6227981112a
Oycctem <ousstemm@gmail.com>
2023-12-05 13:08:35 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """Function that writes a string to a text file and returns the number of characters written"""
 
 
-def read_file(filename="", text=""):
+def write_file(filename="", text=""):
     """writes a string to a text file"""
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
