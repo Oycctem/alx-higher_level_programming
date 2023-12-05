8f1a0fe24b302681920ee2af10c08b3d475633e7
Oycctem <ousstemm@gmail.com>
2023-12-05 14:30:39 -0800

    task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts the texts"""
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += l
+            if search_string in l:
+                text += NewStr
+    with open(filename, "w") as w:
+        w.write(text)
+
