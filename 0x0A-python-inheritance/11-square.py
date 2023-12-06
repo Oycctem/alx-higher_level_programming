f27585ec43e8ef22434a0e6bb67e5008d082c346
Oycctem <ousstemm@gmail.com>
2023-12-06 12:07:12 -0800

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a class that inherits from Rectangle"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        """width, height"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """method called for"""
+        return f"[Square] {self.__size}/{self.__size}"
