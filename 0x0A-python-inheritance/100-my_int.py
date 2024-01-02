a5d213a1c85a0ec02d8aebb052af472743676090
Oycctem <ousstemm@gmail.com>
2024-01-01 17:37:05 -0800

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """a class that inherits from int"""
 
-class myint(int):
+
+class MyInt(int):
     """integer class inverting == and != operators"""
 
     def __eq__(self, value):
         """Override == to behave like !="""
-        return self.read != value
+        return self.real != value
 
     def __ne__(self, value):
         """Override != to behave like =="""
