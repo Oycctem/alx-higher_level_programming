92555e961a96631240fee0f0e1d66b2356f0af6c
Oycctem <ousstemm@gmail.com>
2024-01-01 17:20:33 -0800

    task 10

--- 0x08-python-more_classes/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Function that reads a text file and prints it to stdout"""
-
-
-def read_file(file=""):
-    """reads a test file and prints it to stdout"""
-    with open(file, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
