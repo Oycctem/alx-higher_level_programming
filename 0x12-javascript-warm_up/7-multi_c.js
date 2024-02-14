c0798e6f46b62879b5ae064455352064b5491510
Oycctem <ousstemm@gmail.com>
2024-02-14 14:36:50 -0800

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 let i = 0;
 const count = parseInt(process.argv[2]);
 if (isNaN(process.argv[2])) {
-	console.log('Missing number of occurences');
+	console.log('Missing number of occurrences');
 } else {
 while (i < count) {
-	console.log('test');
+	console.log('C is fun');
 	i++;
 }
 }
