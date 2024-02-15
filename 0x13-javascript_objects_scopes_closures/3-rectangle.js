ba5c9fcff35bb06a7b12b06aa8c6187fcff31d52
Oycctem <ousstemm@gmail.com>
2024-02-15 12:15:33 -0800

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let s = '';
+    let a = 0;
+    while (a < this.height) {
+      const b = 0;
+      while (b < this.width) {
+        s += '\n';
+        a++;
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Rectangle;
