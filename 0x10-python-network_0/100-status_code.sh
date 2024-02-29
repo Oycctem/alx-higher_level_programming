2a3bd12b685ecc17a0aa4674b532c6bc08391059
Oycctem <ousstemm@gmail.com>
2024-02-29 15:56:02 -0800

    task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that sends a request to a URL as an arg then displays the status code
-curl -so /dev/null -w "%{http_code}" $1
+curl -s -o /dev/null -w "%{http_code}" $1
