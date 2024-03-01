106b8e13acc74839da5c46d762564fae0eacf2bf
Oycctem <ousstemm@gmail.com>
2024-03-01 00:37:41 -0800

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""send a request to the URL and display the value of the X-Request-Id"""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+except Exception as e:
+    print("Error fetching URL:", e)
