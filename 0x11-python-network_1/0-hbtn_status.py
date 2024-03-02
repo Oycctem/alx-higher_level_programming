5204c6825d462612176db0f7904d7b4286c00e04
Oycctem <ousstemm@gmail.com>
2024-03-02 01:35:07 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,10 @@ import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 
-try:
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        utf8_content = html.decode('utf-8')
-        print("- Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(utf8_content))
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    utf8_content = html.decode('utf-8')
+    print("- Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(utf8_content))
