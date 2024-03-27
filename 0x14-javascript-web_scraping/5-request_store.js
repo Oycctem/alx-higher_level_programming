7cb4d550ccb265ef105c5f36fafd2163733f446b
Oycctem <ousstemm@gmail.com>
2024-03-27 13:12:05 +0000

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,5 @@ const fs = require('fs');
 const request = require('request');
 request.get(process.argv[2], { encoding: 'utf-8' }, (_, response, body) => {
   fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-    if (err) console.error(err);
-    else console.log(`Successfully stored the contents of ${process.argv[2]} in ${process.argv[3]}`);
   });
 });
