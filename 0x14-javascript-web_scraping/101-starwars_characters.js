e7e9e06c6f828ad292f499ae4aaee7fa7f4b3e42
Oycctem <ousstemm@gmail.com>
2024-03-27 02:00:34 +0000

    task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,11 +6,19 @@ if (process.argv.length < 3) {
 }
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const fetchCharacterDetails = (characterUrl) => {
+    return new Promise((resolve) => {
+        request.get(characterUrl, (_, __, body) => {
+            resolve(JSON.parse(body).name);
+        });
+    });
+};
 request.get(apiUrl, (_, response, body) => {
     const movie = JSON.parse(body);
     movie.characters.forEach(characterUrl => {
-        request.get(characterUrl, (_, __, body) => {
-            console.log(JSON.parse(body).name);
-        });
+        fetchCharacterDetails(characterUrl)
+            .then(characterName => {
+                console.log(characterName);
+            });
     });
 });
